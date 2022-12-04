ea8dca18becde8976f7b24b1df4b02bd730e30ed
Avo_Sukiasyan <avo.sukaisyan@gmail.com>
2022-12-04 13:13:20 +0400

    fixed minor issue on the metadata

--- docs/metaMap-reactNative.md
+++ docs/metaMap-reactNative.md
@@ -8,7 +8,7 @@ category: 61ae8e8dba577a0010791480
 
 | LTS version (Recommended for most users): | Current Version(Latest features) |
 |-------------------------------------------|----------------------------------|
-| 4.7.2                                     | 4.7.2                            |
+| 4.8.0                                     | 4.8.3                            |
 
 ## Install MetaMap for React Native Expo
 
--- ios/MetaMapRNSdk.m
+++ ios/MetaMapRNSdk.m
@@ -10,7 +10,8 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(showFlow:(NSString * _Nonnull)clientId flowId:(NSString * _Nullable)flowId metadata:(NSDictionary<NSString *, id> * _Nullable)metadata)
 {
     dispatch_async(dispatch_get_main_queue(), ^(void){
-        metadata["sdkType"] = "react-native-ios";
+        NSMutableDictionary *mutableDictionary = [metadata mutableCopy];     //Make the dictionary mutable to change/add
+        mutableDictionary[@"sdkType"] = @"react-native-ios";
         [MetaMap.shared showMetaMapFlowWithClientId: clientId flowId: flowId metadata: metadata];
         [MetaMapButtonResult shared].delegate = self;
         self->hasListeners = YES;
