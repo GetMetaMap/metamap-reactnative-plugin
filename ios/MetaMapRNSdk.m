9f201756ecf03c1ba58c2ae0365fe35dfa914bdd
Avo Sukiasyan <avetik.sukiasyan@incode.com>
2025-05-07 17:51:03 +0400

    Update MetaMapRNSdk.m

--- ios/MetaMapRNSdk.m
+++ ios/MetaMapRNSdk.m
@@ -1,118 +1,134 @@
 #import "MetaMapRNSdk.h"
+#import <React/RCTEventEmitter.h>
 #import <React/RCTUtils.h>
 
-static NSString *const kEventVerificationSuccess = @"verificationSuccess";
+static NSString *const kEventVerificationSuccess  = @"verificationSuccess";
 static NSString *const kEventVerificationCanceled = @"verificationCanceled";
-static NSString *const kEventVerificationStarted = @"verificationStarted";
+static NSString *const kEventVerificationStarted  = @"verificationStarted";
 
-@interface MetaMapRNSdk () <MetaMapButtonResultDelegate>
-@property(nonatomic, assign) BOOL hasListeners;
-@end
-
-@implementation MetaMapRNSdk
+@implementation MetaMapRNSdk {
+  BOOL _hasListeners;
+  NSInteger _listenerCount;
+}
 
 RCT_EXPORT_MODULE()
 
-#pragma mark - React Native API
-
-RCT_EXPORT_METHOD(showFlow
-:(NSString *)
-clientId
-        flowId
-:(NSString *)
-flowId
-        metadata
-:(NSDictionary *)metadata)
+#pragma mark - RN 0.71+ listener contract
+- (void)addListener:(NSString *)eventName
 {
-    [self startFlowWithClientId:clientId
-                         flowId:flowId
-                configurationId:nil
-      encryptionConfigurationId:nil
-                       metadata:metadata];
+  [super addListener:eventName];
+  _listenerCount += 1;
+  _hasListeners = YES;
 }
 
-RCT_EXPORT_METHOD(showFlowWithConfigurationId
-:(NSString *)
-clientId
-        flowId
-:(NSString *)
-flowId
-        configurationId
-:(NSString *)
-configurationId
-        encryptionConfigurationId
-:(NSString *)
-encryptionConfigurationId
-        metadata
-:(NSDictionary *)metadata)
+- (void)removeListeners:(double)count
 {
-    [self startFlowWithClientId:clientId
-                         flowId:flowId
-                configurationId:configurationId
-      encryptionConfigurationId:encryptionConfigurationId
-                       metadata:metadata];
+  [super removeListeners:count];
+  _listenerCount -= (NSInteger)count;
+  if (_listenerCount <= 0) {
+    _listenerCount = 0;
+    _hasListeners = NO;
+  }
 }
 
-#pragma mark - Private helpers
+#pragma mark - Required overrides
++ (BOOL)requiresMainQueueSetup
+{
+  // We show a native UI immediately; initialise on main.
+  return YES;
+}
 
+- (NSArray<NSString *> *)supportedEvents
+{
+  return @[
+    kEventVerificationSuccess,
+    kEventVerificationCanceled,
+    kEventVerificationStarted
+  ];
+}
+
+- (void)invalidate
+{
+  // Called when the bridge is deallocated (e.g. hot reload)
+  _hasListeners = NO;
+  _listenerCount = 0;
+}
+
+#pragma mark - React‑exposed API
+RCT_EXPORT_METHOD(showFlow:(NSString *)clientId
+                  flowId:(NSString *)flowId
+                  metadata:(NSDictionary *)metadata)
+{
+  [self startFlowWithClientId:clientId
+                       flowId:flowId
+              configurationId:nil
+    encryptionConfigurationId:nil
+                     metadata:metadata];
+}
+
+RCT_EXPORT_METHOD(showFlowWithConfigurationId:(NSString *)clientId
+                  flowId:(NSString *)flowId
+                  configurationId:(NSString *)configurationId
+                  encryptionConfigurationId:(NSString *)encryptionConfigurationId
+                  metadata:(NSDictionary *)metadata)
+{
+  [self startFlowWithClientId:clientId
+                       flowId:flowId
+              configurationId:configurationId
+    encryptionConfigurationId:encryptionConfigurationId
+                     metadata:metadata];
+}
+
+#pragma mark - Internal helper
 - (void)startFlowWithClientId:(NSString *)clientId
                        flowId:(NSString *)flowId
               configurationId:(NSString *)configurationId
     encryptionConfigurationId:(NSString *)encryptionConfigurationId
-                     metadata:(NSDictionary *)metadata {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        NSMutableDictionary *mutableMetadata = metadata
-                                               ? [metadata mutableCopy]
-                                               : [NSMutableDictionary dictionary];
-        mutableMetadata[@"sdkType"] = @"react-native-ios";
-
-        [MetaMap.shared showMetaMapFlowWithClientId:clientId
-                                             flowId:flowId
-                                    configurationId:configurationId
-                          encryptionConfigurationId:encryptionConfigurationId
-                                           metadata:mutableMetadata];
-
-        [MetaMapButtonResult shared].delegate = self;
-        self.hasListeners = YES;
-    });
-}
-
-- (NSArray
-
-<NSString *> *)supportedEvents
+                     metadata:(NSDictionary *)metadata
 {
-    return @[kEventVerificationSuccess,
-             kEventVerificationCanceled,
-             kEventVerificationStarted];
+  dispatch_async(dispatch_get_main_queue(), ^{
+    NSMutableDictionary *mutableMetadata =
+        metadata ? [metadata mutableCopy] : [NSMutableDictionary dictionary];
+    mutableMetadata[@"sdkType"] = @"react-native-ios";
+
+    [MetaMap.shared showMetaMapFlowWithClientId:clientId
+                                         flowId:flowId
+                                configurationId:configurationId
+                      encryptionConfigurationId:encryptionConfigurationId
+                                       metadata:mutableMetadata];
+
+    [MetaMapButtonResult shared].delegate = self;
+  });
 }
 
 #pragma mark - MetaMapButtonResultDelegate
-
 - (void)verificationCreatedWithIdentityId:(NSString *)identityId
-                           verificationID:(NSString *)verificationID {
-    if (self.hasListeners) {
-        [self sendEventWithName:kEventVerificationStarted
-                           body:@{@"identityId": identityId ?: [NSNull null],
-                                  @"verificationId": verificationID ?: [NSNull null]}];
-    }
+                           verificationID:(NSString *)verificationID
+{
+  if (_hasListeners) {
+    [self sendEventWithName:kEventVerificationStarted
+                       body:@{@"identityId": identityId ?: [NSNull null],
+                              @"verificationId": verificationID ?: [NSNull null]}];
+  }
 }
 
 - (void)verificationSuccessWithIdentityId:(NSString *)identityId
-                           verificationID:(NSString *)verificationID {
-    if (self.hasListeners) {
-        [self sendEventWithName:kEventVerificationSuccess
-                           body:@{@"identityId": identityId ?: [NSNull null],
-                                  @"verificationId": verificationID ?: [NSNull null]}];
-    }
+                           verificationID:(NSString *)verificationID
+{
+  if (_hasListeners) {
+    [self sendEventWithName:kEventVerificationSuccess
+                       body:@{@"identityId": identityId ?: [NSNull null],
+                              @"verificationId": verificationID ?: [NSNull null]}];
+  }
 }
 
 - (void)verificationCancelledWithIdentityId:(NSString *)identityId
-                             verificationID:(NSString *)verificationID {
-    if (self.hasListeners) {
-        [self sendEventWithName:kEventVerificationCanceled
-                           body:@{@"identityId": identityId ?: [NSNull null],
-                                  @"verificationId": verificationID ?: [NSNull null]}];
-    }
+                             verificationID:(NSString *)verificationID
+{
+  if (_hasListeners) {
+    [self sendEventWithName:kEventVerificationCanceled
+                       body:@{@"identityId": identityId ?: [NSNull null],
+                              @"verificationId": verificationID ?: [NSNull null]}];
+  }
 }
-
 @end
